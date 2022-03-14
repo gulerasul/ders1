9270d179436ee74454508186e0cbec13aa40791d
Guler Resulova <tu699evkp@aztu.local>
2022-03-14 16:19:31 +0400

    first commit

---
+++ homework/homework/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework
+{
+    class Car:Vehicle
+    {
+        public string brand;
+        public string model;
+        public double FuelCapacity;
+        public double FuelFor1km;
+        public double CurrentFuel;
+
+        public Car(int year,string brand,string model,double FuelCapacity,double FuelFor1km):base(year) 
+        {
+            this.brand = brand;
+            this.model = model;
+            this.FuelCapacity = FuelCapacity;
+            this.FuelFor1km = FuelFor1km;
+        }
+        public Car(int year, string brand, string model, double FuelCapacity, double FuelFor1km,double CurrentFuel):this(year,brand,model,FuelCapacity,FuelFor1km)
+        {
+            this.CurrentFuel = CurrentFuel;
+        }
+        public Car(int year, string brand, string model, double FuelCapacity, double FuelFor1km, double CurrentFuel,string color):this(year, brand, model, FuelCapacity, FuelFor1km,CurrentFuel )
+        {
+            this.color = color;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Mashinin markasi :" +brand);
+            Console.WriteLine("Mashinin modeli :" + model);
+            Console.WriteLine("Mashinin bezin tutumu  :" + FuelCapacity);
+            Console.WriteLine("Mashinin 1km uchun yanacaq serfiyyati :" + FuelFor1km);
+            Console.WriteLine("Mashinin movcud olan yanacaq :" + CurrentFuel);
+            Console.WriteLine("Mashinin rengi :"+color);
+        }
+
+
+    }
+    
+}
---
+++ homework/homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(2016,"bmw","m5",50,0.2,15,"qara");
+            car.ShowInfo();
+        }
+    }
+}
